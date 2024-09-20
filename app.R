42ba4fe9a7d49496290371a67d7980f87566529d
Behavioural-Scientist <julian.house@ontario.ca>
2024-09-20 15:26:41 -0400

    Added theme and panels to UI.

--- app.R
+++ app.R
@@ -1,51 +1,112 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
-library(shiny)
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+library(tidyverse
+        , shiny
         )
-    )
-)
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+Income_Calc <- function(Wage
+                        , Hours) {
+  Wage * Hours
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+ui <- fluidPage(
+  theme = bslib::bs_theme(bootswatch = "flatly"),
+  tabsetPanel(
+    tabPanel("Introduction",
+             titlePanel("Social Assistance Income Calculator"),
+             textOutput("Intro")
+             ),
+    tabPanel("About Me",
+             fluidRow(
+               column(3
+                      , textOutput("Scenario_1")
+                      , numericInput("Wage_1"
+                                     , "Wage"
+                                     , value = 17.20
+                                     , min = 17.20
+                                     , max = 100
+                      )
+                      , numericInput("Hours_1"
+                                     , "You can change hours here to see another employment scenario"
+                                     , value = 0
+                                     , min = 0
+                                     , max = 60
+                      )
+               )
+               , column(3
+                        , textOutput("Scenario_2")
+                        , numericInput("Wage_2"
+                                       , "Wage"
+                                       , value = 17.20
+                                       , min = 17.20
+                                       , max = 100
+                        )
+                        , numericInput("Hours_2"
+                                       , "Part-time hours could me more or less than 17 per week"
+                                       , value = 17
+                                       , min = 0
+                                       , max = 60
+                        )
+               )
+               , column(3
+                        , textOutput("Scenario_3")
+                        , numericInput("Wage_3"
+                                       , "Wage"
+                                       , value = 17.20
+                                       , min = 17.20
+                                       , max = 100
+                        )
+                        , numericInput("Hours_3"
+                                       , "Full-time typically starts at 30 hours per week. Overtime starts after 44 hours per week"
+                                       , value = 35
+                                       , min = 0
+                                       , max = 60
+                        )
+               )
+             )
+             , actionButton("Calculate"
+                            , "Calculate"
+             ) 
+             ),
+    tabPanel("Income Results",
+             fluidRow(
+               column(9
+                      , plotOutput("Income_Plot"
+                      )
+               )
+             )
+             )
+  )
+)
+server <- function(input, output, session) {
+  output$Scenario_1 <- renderText("Unemployed")
+  output$Scenario_2 <- renderText("Employed Part Time")
+  output$Scenario_3 <- renderText("Employed Full Time")
+  Calcd_Income_1 <- eventReactive(input$Calculate, {
+    Income_Calc(input$Wage_1
+                 , input$Hours_1
+    )
+    })
+    Calcd_Income_2 <- eventReactive(input$Calculate, {
+      Income_Calc(input$Wage_2
+                  , input$Hours_2
+                  )
+                                                      }
+                                    )
+    Calcd_Income_3 <- eventReactive(input$Calculate, {
+      Income_Calc(input$Wage_3
+                  , input$Hours_3
+                   )
+    })
+    Income_Tibble <- reactive(tibble(Calcd_Income = c(Calcd_Income_1()
+                                                          , Calcd_Income_2()
+                                                          , Calcd_Income_3()
+                                                          )
+                                     , Scenario = c(1:3)
+                                     )
+                              )
+    output$Income_Plot <- renderPlot({
+      ggplot(Income_Tibble(), aes(x = Scenario, y = Calcd_Income)) +
+        geom_col()
+    }, res = 96
+    )
+}
+shinyApp(ui, server)
