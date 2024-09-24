be5e9785d401012842cda770ed19be1fb926551d
Behavioural-Scientist <julian.house@ontario.ca>
2024-09-24 13:30:53 -0400

    Added navigation bar wrapper to tabs at top to take advantage of themes. Also added warning messages around minimum wage values.

--- app.R
+++ app.R
@@ -1,27 +1,39 @@
-library(tidyverse
-        , shiny
-        )
+# Packages
+library(tidyverse,
+        readr)
+library(shiny, 
+        shinyFeeedback)
+library(scales)
 
+# Constants
+Min_Wage <- 17.2
+# Functions
 Income_Calc <- function(Wage
                         , Hours) {
   Wage * Hours
 }
 
 ui <- fluidPage(
-  theme = bslib::bs_theme(bootswatch = "flatly"),
-  tabsetPanel(
+  # Setting the look and feel of the app.
+  theme = bslib::bs_theme(bootswatch = "united"),
+  # Setting up the ability to provide dynamic user feedback.
+  shinyFeedback::useShinyFeedback(),
+  # Creating tabs to organize the content of the shiny app. 
+  navbarPage(
+    titlePanel("Social Assistance Income Calculator"),
     tabPanel("Introduction",
-             titlePanel("Social Assistance Income Calculator"),
              textOutput("Intro")
              ),
     tabPanel("About Me",
+             ),
+    tabPanel("Work Scenarios",
              fluidRow(
                column(3
                       , textOutput("Scenario_1")
                       , numericInput("Wage_1"
                                      , "Wage"
                                      , value = 17.20
-                                     , min = 17.20
+                                     , min = 0
                                      , max = 100
                       )
                       , numericInput("Hours_1"
@@ -36,7 +48,7 @@ ui <- fluidPage(
                         , numericInput("Wage_2"
                                        , "Wage"
                                        , value = 17.20
-                                       , min = 17.20
+                                       , min = 0
                                        , max = 100
                         )
                         , numericInput("Hours_2"
@@ -51,7 +63,7 @@ ui <- fluidPage(
                         , numericInput("Wage_3"
                                        , "Wage"
                                        , value = 17.20
-                                       , min = 17.20
+                                       , min = 0
                                        , max = 100
                         )
                         , numericInput("Hours_3"
@@ -62,9 +74,9 @@ ui <- fluidPage(
                         )
                )
              )
-             , actionButton("Calculate"
-                            , "Calculate"
-             ) 
+             # , actionButton("Calculate"
+             #                , "Calculate"
+             # ) 
              ),
     tabPanel("Income Results",
              fluidRow(
@@ -73,25 +85,62 @@ ui <- fluidPage(
                       )
                )
              )
-             )
+             ),
+    tabPanel("More Information")
   )
 )
 server <- function(input, output, session) {
+  # Error messages
+  observeEvent(input$Wage_1, {
+    shinyFeedback::feedbackWarning("Wage_1",
+                                   input$Wage_1 < Min_Wage,
+                                   paste0("Ontario minimum wage is ",
+                                          dollar(Min_Wage),
+                                         " per hour."
+                                          )
+                                   )
+  })
+  observeEvent(input$Wage_2, {
+    shinyFeedback::feedbackWarning("Wage_2",
+                                   input$Wage_2 < Min_Wage,
+                                   paste0("Ontario minimum wage is ",
+                                          dollar(Min_Wage),
+                                          " per hour."
+                                   )
+    )
+  })
+  observeEvent(input$Wage_3, {
+    shinyFeedback::feedbackWarning("Wage_3",
+                                   input$Wage_3 < Min_Wage,
+                                   paste0("Ontario minimum wage is ",
+                                          dollar(Min_Wage),
+                                          " per hour."
+                                   )
+    )
+  })
+  output$Intro <- renderText("This app is designed to help you understand how your income changes based on your employment status.")
   output$Scenario_1 <- renderText("Unemployed")
   output$Scenario_2 <- renderText("Employed Part Time")
   output$Scenario_3 <- renderText("Employed Full Time")
-  Calcd_Income_1 <- eventReactive(input$Calculate, {
-    Income_Calc(input$Wage_1
-                 , input$Hours_1
+  Calcd_Income_1 <- reactive({
+    # Legal_Wage <- input$Wage_1 >= 17.2
+    # shinyFeedback::feedbackWarning("Wage_1", 
+    #                                !Legal_Wage, 
+    #                                "Ontario minimum wage is $17.20 per hour."
+    #                                )
+    # req(Legal_Wage, cancelOutput = TRUE)
+    
+    Income_Calc(input$Wage_1,
+                input$Hours_1
     )
     })
-    Calcd_Income_2 <- eventReactive(input$Calculate, {
+    Calcd_Income_2 <- reactive({
       Income_Calc(input$Wage_2
                   , input$Hours_2
                   )
                                                       }
                                     )
-    Calcd_Income_3 <- eventReactive(input$Calculate, {
+    Calcd_Income_3 <- reactive({
       Income_Calc(input$Wage_3
                   , input$Hours_3
                    )
